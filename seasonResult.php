f6c86960c3586a0f3aa31c5b5bc5b7e8c9d16c84
Petter L <plerenius@gmail.com>
2020-05-07 10:06:47 +0200

    Fixat syntax-varningar

--- seasonResult.php
+++ seasonResult.php
@@ -3,22 +3,34 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Result</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity=
+        "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    >
+    <link rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+      integrity=
+        "sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
+      crossorigin="anonymous"
+    >
     <link href="style.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <nav class="navbar bg-dark navbar-dark">
       <div class="container">
-        <a href="" class="navbar-brand"><i class="fas fa-golf-ball mr-3"></i>Oban Tour</a>
+        <a href="" class="navbar-brand">
+          <i class="fas fa-golf-ball mr-3"></i>Oban Tour
+        </a>
       </div>
     </nav>
     <section id="result" class="container-fluid px-0">
       <div class="row justify-content-center">
         <div class="col-8">
           <?php
-            include "inc/SeasonResult_inc.php";
-            printTables();
+              require "inc/SeasonResult_inc.php";
+              printTables();
           ?>
         </div>
       </div>
