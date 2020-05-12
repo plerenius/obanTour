b9bb3d2fd5b89ccaa4d60d43e2e5b55c5e025f82
Petter L <plerenius@gmail.com>
2020-05-12 10:21:16 +0200

    Fixade fel med sed

--- update_tags.sh
+++ update_tags.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Update tags in repo..."
-git grep -l ":git_id:" | xargs sed -e "s/:git_id:/`git describe --abbrev=10 --dirty --always --tags`/"
-git grep -l ":pub_date:" | xargs sed -e "s/:pub_date:/$(date '+%Y-%m-%d %H:%M')/"
+git grep -l ":git_id:" | xargs sed -i "s/:git_id:/`git describe --abbrev=10 --dirty --always --tags`/"
+git grep -l ":pub_date:" | xargs sed -i "s/:pub_date:/$(date '+%Y-%m-%d %H:%M')/"
