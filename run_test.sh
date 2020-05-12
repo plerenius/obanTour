c00e0f8dbfdd5a899bcaf8c347993558e165eb5a
Petter L <plerenius@gmail.com>
2020-05-12 12:35:23 +0200

    skapa test script

---
+++ run_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Update tags in repo..."
+git grep -l ":git_id:" | xargs sed -i "s/:git_id:/`git describe --abbrev=10 --dirty --always --tags`/"
+git grep -l ":pub_date:" | xargs sed -i "s/:pub_date:/$(date '+%Y-%m-%d %H:%M')/"
+
+echo "Run unittests..."
+phpunit --bootstrap ./autoload.php --testdox ./test/
